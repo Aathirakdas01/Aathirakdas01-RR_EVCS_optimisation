3830c1b20417333807f8b03d5f501f8464cb2214
Aathira Krishndas <aakrishn@tcd.ie>
2025-08-13 09:54:38 +0100

    Add functions

---
+++ scripts/config.py
@@ -0,0 +1,19 @@
+# -------------------------------
+# Constants and model settings
+# -------------------------------
+
+# File paths
+NETWORK_CSV_PATH = "data/network.csv"
+DEMAND_CSV_PATH = "data/ev_demand.csv"
+
+# Maximum distance covered
+COVERAGE_DIST = 20
+
+# Maximum number of stations allowed
+MAX_STATIONS = 10
+
+# Station installation cost
+FIXED_COST = 1000
+
+# Cost per unit demand per unit distance
+VARIABLE_COST = 2
